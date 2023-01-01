b3d9b751689631b1c4f1a33513c0bd33f6b3a2a1
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-01-01 14:56:26 +0100

    post CRUD completed

--- controllers/post.js
+++ controllers/post.js
@@ -1,26 +1,53 @@
-import {Post} from "../model/Post.js";
+import { Post } from "../model/Post.js";
 
+// CREATE FUNDRASIER POST
 export const createFundraiserPost = async (req, res, next) => {
   try {
     const post = new Post({
       ...req.body,
     });
-    await post.save()
-    res.status(200).json('FundRaiser post created')
+    await post.save();
+    res.status(200).json("FundRaiser post created");
   } catch (err) {
-    res.status(400).json(err)
+    res.status(400).json(err);
   }
 };
 
+// UPDATE FUNDRAISER POST
+export const updateFundraiserPost = async (req, res, next) => {
+  try {
+    const post = await Post.findOneAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+// DELETE FUNDRAISER POST
+export const deleteFundraiserPost = async (req, res, next) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json("Post deleted succesfully!");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+// GET ONE FUNDRAISER POST
 export const getOneFundraiserPost = async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.id)
-    res.status(200).json(post)
-  }catch(err){
-    res.status(400).json(err)
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
   }
-}
+};
 
+// GET MANY FUNDRAISER POST
 export const getManyFundraiserPost = async (req, res, next) => {
   try {
     const post = await Post.find();
--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   createFundraiserPost,
   getOneFundraiserPost,
-  getManyFundraiserPost
+  getManyFundraiserPost,
+  updateFundraiserPost,
+  deleteFundraiserPost
 } from "../controllers/post.js";
 
 export const postRouter = express.Router();
@@ -10,3 +12,5 @@ export const postRouter = express.Router();
 postRouter.post("/createFundraiser", createFundraiserPost);
 postRouter.get("/getOneFundraiser/:id", getOneFundraiserPost);
 postRouter.get("/getManyFundraiser", getManyFundraiserPost);
+postRouter.put("/updateFundraiser/:id", updateFundraiserPost);
+postRouter.delete("/deleteFundraiser/:id", deleteFundraiserPost);
