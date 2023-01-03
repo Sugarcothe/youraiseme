f498372936c541a17a726b71bd6f71c37eaccb98
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-01-03 09:42:32 +0100

    strapi

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
 import { userRouter } from "./routes/user.js";
 import { postRouter } from "./routes/post.js";
+import { stripeRouter } from "./routes/stripe.js";
 
 const server = express();
 dotenv.config();
+server.use(cors());
 
 server.use(express.json());
 server.use(cookieParser());
+server.use(bodyParser.json());
 server.use("/api/v1/users", userRouter);
 server.use("/api/v1/posts", postRouter);
+server.use("/payment", stripeRouter);
 
 mongoose.set("strictQuery", false);
 
@@ -23,6 +29,6 @@ try {
 }
 
 server.listen(
-  process.env.PORT,
+  process.env.PORT || 8000,
   console.log(`server listening on ${process.env.PORT}`)
 );
